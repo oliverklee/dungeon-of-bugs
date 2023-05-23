4eab4ba938423d09be12b7537b26613f0c04ca44
Oliver Klee <typo3-coding@oliverklee.de>
2023-05-23 19:01:31 +0200

    [FEATURE] Add PHP-CS-Fixer to our CI toolchain (#8)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()->in(__DIR__ . '/src');
+$config->getFinder()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
 
 return $config;
