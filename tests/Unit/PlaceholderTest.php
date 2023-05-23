bb3afff0fe0aa9891173a5f5266f0080671a61ff
Oliver Klee <typo3-coding@oliverklee.de>
2023-05-23 18:40:47 +0200

    [FEATURE] Add some dummy tests (#5)

---
+++ tests/Unit/PlaceholderTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ */
+final class PlaceholderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function universeIsStillOkay(): void
+    {
+        self::assertSame(2, 1 + 1);
+    }
+}
