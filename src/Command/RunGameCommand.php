d2a215c28f4681d086dea0f327cb4929c9be8113
Oliver Klee <typo3-coding@oliverklee.de>
2023-05-25 19:04:11 +0200

    [FEATURE] Add a basic game runner

---
+++ src/Command/RunGameCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\DungeonOfBugs\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Cursor;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\StreamableInputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Runs the game in Symfony console.
+ */
+#[AsCommand(name: 'game:run')]
+class RunGameCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setDescription('Run the snake game in Symfony console');
+    }
+
+    /**
+     * @return Command::*
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if ($input instanceof StreamableInputInterface && \is_resource($input->getStream())) {
+            $inputStream = $input->getStream();
+        } else {
+            $inputStream = STDIN;
+        }
+
+        \stream_set_blocking($inputStream, false);
+        $sttyMode = \shell_exec('stty -g');
+        \shell_exec('stty -icanon -echo');
+
+        $cursor = new Cursor($output);
+        $cursor->hide();
+
+        $output->writeln('Hello World!');
+
+        $cursor->show();
+        \stream_set_blocking($inputStream, true);
+        \shell_exec(sprintf('stty %s', $sttyMode));
+
+        return Command::SUCCESS;
+    }
+}
