44b35eb30efc82a5dd9c28cade1440d4946e2231
Oliver Klee <typo3-coding@oliverklee.de>
2023-05-29 22:38:41 +0200

    [FEATURE] Add base classes (#28)

--- src/Command/RunGameCommand.php
+++ src/Command/RunGameCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OliverKlee\DungeonOfBugs\Command;
 
+use OliverKlee\DungeonOfBugs\GameBuilder;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Cursor;
@@ -35,15 +36,31 @@ class RunGameCommand extends Command
             $inputStream = STDIN;
         }
 
+        $levelFile = $input->getArgument('levelfile');
+        \assert(\is_string($levelFile));
+
         \stream_set_blocking($inputStream, false);
         $sttyMode = \shell_exec('stty -g');
         \shell_exec('stty -icanon -echo');
 
         $cursor = new Cursor($output);
         $cursor->hide();
+        $cursor->clearScreen();
 
+        $cursor->moveToPosition(1, 1);
         $output->writeln('Hello World!');
-        $output->writeln('Level to load: ' . $input->getArgument('levelfile'));
+        $output->writeln('Level to load: ' . $levelFile);
+
+        $gameBuilder = new GameBuilder();
+        $world = $gameBuilder->loadGameWorld($levelFile);
+        $game = $gameBuilder->buildGame($world, $input, $output);
+        $cursor->moveToPosition(1, 10);
+
+        do {
+            $output->writeln('Here we will see the game world.');
+            $output->writeln('It will have multiple lines');
+            // copy key input handling from https://github.com/dbu/php-snake/blob/main/src/Game/Game.php
+        } while ($game->isRunning());
 
         $cursor->show();
         \stream_set_blocking($inputStream, true);
